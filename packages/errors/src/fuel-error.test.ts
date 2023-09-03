a81cc62232f9e30ef0609d4117ec49af7a49a7d9
nedsalk <nedim.salkic@fuel.sh>
2023-09-03 19:31:58 +0200

    test: added some tests

--- packages/errors/src/fuel-error.test.ts
+++ packages/errors/src/fuel-error.test.ts
@@ -1,16 +1,17 @@
 import { versions } from '@fuel-ts/versions';
 
 import { FuelError, ErrorCode } from '.';
+import { errorMessages } from './error-messages';
 import { expectToThrowFuelError } from './test-utils/expect-to-throw-fuel-error';
 
 it('has properties set as expected on creation', () => {
-  const message = 'whatever';
-  const error = new FuelError(FuelError.CODES.PARSE_FAILED, message);
+  const error = new FuelError(FuelError.CODES.PARSE_FAILED, 'unknown-error-code', { prop: 'prop' });
 
-  expect(error.message).toBe(message);
+  expect(error.message).toBe(errorMessages[error.code]['unknown-error-code']);
   expect(error.code).toBe(ErrorCode.PARSE_FAILED);
   expect(error.name).toBe('FuelError');
   expect(error.VERSIONS).toStrictEqual(versions);
+  expect(error.additionalInfo).toStrictEqual({ prop: 'prop' });
 });
 
 describe('Parsing', () => {
@@ -23,16 +24,13 @@ describe('Parsing', () => {
   });
 
   it('fails when parsing an object without a code property', () => {
-    const expectedError = new FuelError(FuelError.CODES.PARSE_FAILED, "missing 'code' property");
+    const expectedError = new FuelError(FuelError.CODES.PARSE_FAILED, 'missing-code-property');
     expectToThrowFuelError(() => FuelError.parse({}), expectedError);
   });
 
   it('fails when parsing an object with an unknown error code', () => {
     const code = 'qweqwe';
-    const expectedError = new FuelError(
-      ErrorCode.PARSE_FAILED,
-      `Unknown error code: ${code}. Accepted codes: ${Object.values(ErrorCode).join(', ')}.`
-    );
+    const expectedError = new FuelError(ErrorCode.PARSE_FAILED, 'unknown-error-code');
     expectToThrowFuelError(() => FuelError.parse({ code }), expectedError);
   });
 });
@@ -40,7 +38,13 @@ describe('Parsing', () => {
 it('converts error to plain object', () => {
   const code = FuelError.CODES.PARSE_FAILED;
   const name = 'FuelError';
-  const message = 'It happens';
-  const err = new FuelError(code, message);
-  expect(err.toObject()).toEqual({ code, name, message, VERSIONS: err.VERSIONS });
+  const err = new FuelError(code, 'missing-code-property', { prop123: 'asdf' });
+  expect(err.toObject()).toStrictEqual({
+    code,
+    name,
+    message: "missing 'code' property.",
+    VERSIONS: err.VERSIONS,
+    messageKey: 'missing-code-property',
+    additionalInfo: { prop123: 'asdf' },
+  });
 });
